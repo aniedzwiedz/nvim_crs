da60805daf4aea7bb40c84533dc0d541a59bb1d3
Niedzwiedz Artur <artur.niedzwiedz@igt.com>
2024-02-03 17:45:26 +0100

    diffview: keymaps

--- lua/user/diffview.lua
+++ lua/user/diffview.lua
@@ -2,8 +2,16 @@ local M = {
   "sindrets/diffview.nvim",
   event = "VeryLazy",
   cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+  keys = {
+    { "<F4>", ":DiffviewClose <cr>", desc = "Close Diff View" }, -- closing Diffview
+  },
 }
 
-function M.config() end
+function M.config()
+  local wk = require "which-key"
+  wk.register {
+    ["<leader>gd"] = { "<cmd>DiffviewOpen <cr>", "Open DiffviewOpen" },
+  }
+end
 
 return M
